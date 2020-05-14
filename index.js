a411e4bc7a36587340d1f46090e303d29a0a168a
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-14 17:14:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function appendKitten(name){
 //prepends a kitten to the beginning of the kittens array and returns a new array, leaving the kittens array unchanged
 function prependKitten(name){
 
- 
+  kittens.concat(["Milo", "Otis", "Garfield"], name)
 
 }
 
